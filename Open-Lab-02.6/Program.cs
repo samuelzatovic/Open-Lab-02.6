6595cd095754f28acd83a66f2cedd1d0e58c5d09
samuel zaťovič <szatovic@gmail.com>
2019-10-20 10:08:30 +0200

    Hotový program

---
+++ Open-Lab-02.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Open_Lab_02._6
+{
+    class Program
+    {
+        public static bool Divisible(int Dividend)
+        {
+            if (Dividend % 100 == 0)
+                
+                return true;
+            else
+
+                return false;
+        }
+    }
+}
